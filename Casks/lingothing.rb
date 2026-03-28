05f9c1ca3aa64f62c8b45c9f757e5b34cc289e92
nhestrompia <nhestrompia@gmail.com>
2026-03-28 18:47:18 +0200

    release: v0.1.31

--- Casks/lingothing.rb
+++ Casks/lingothing.rb
@@ -1,6 +1,6 @@
 cask "lingothing" do
-  version "0.1.3"
-  sha256 "ea7c79a88e55df8926bc72b07b3c9b54384b68859745e3ab770c9335a0087fa8"
+  version "0.1.31"
+  sha256 "88cddfc3b8025495aa5bda8c8f3fae081fe8b7f01826b2c7f5d9dd0f1f04917e"
 
   url "https://github.com/nhestrompia/lingothing/releases/download/v#{version}/LingoThing-#{version}.app.zip"
   name "LingoThing"
